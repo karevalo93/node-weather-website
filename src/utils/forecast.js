3f74306041471d4ffe3b952df406b58ff28eca37
karevalo93 <kevin.arevalo.l@hotmail.com>
2020-11-20 19:01:53 -0500

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,26 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=b5f13d469d7625cd18acaf25e242bc1c&query=' + latitude + ',' + longitude + '8&units=m'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        }
+        else if (body.error) {
+            callback('Unable to find location,Try another search', undefined)
+        }
+        else {
+            callback(undefined,
+                body.current.weather_descriptions[0] +
+                'It is currently ' + body.current.temperature + ' degree out. It feels like ' +
+                body.current.feelslike + ' degree out.'
+
+            )
+
+        }
+    })
+
+}
+
+module.exports = forecast
